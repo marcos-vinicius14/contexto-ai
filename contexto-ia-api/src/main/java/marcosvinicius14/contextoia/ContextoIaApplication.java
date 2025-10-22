0033b32236e5d7ac5cf407765a8c7cdea435bb22
marcos <marcos.vinicius.dev3@gmail.com>
2025-10-22 19:27:30 -0300

    inital commit

---
+++ contexto-ia-api/src/main/java/marcosvinicius14/contextoia/ContextoIaApplication.java
@@ -0,0 +1,13 @@
+package marcosvinicius14.contextoia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextoIaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContextoIaApplication.class, args);
+    }
+
+}
---
+++ contexto-ia-api/src/test/java/marcosvinicius14/contextoia/ContextoIaApplicationTests.java
@@ -0,0 +1,13 @@
+package marcosvinicius14.contextoia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextoIaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
