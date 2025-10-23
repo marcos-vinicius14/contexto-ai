13ab034020ba2ef3e58c48a9c07bb639d90b0172
marcos <marcos.vinicius.dev3@gmail.com>
2025-10-23 19:33:09 -0300

    feat: finish migrations

---
+++ contexto-ia-api/app/src/main/resources/db/migration/V1__Create_table_users_and_documents.sql
@@ -0,0 +1,28 @@
+CREATE TABLE tb_users (
+    id UUID PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE
+);
+
+-- Sugestão de correção
+CREATE TABLE tb_documents(
+    id UUID PRIMARY KEY,
+    file_name VARCHAR(400) NOT NULL,
+    storage_key VARCHAR(1024) NOT NULL,
+    status VARCHAR(50) NOT NULL DEFAULT 'PENDING',
+    user_id UUID NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES tb_users(id)
+);
+
+CREATE TABLE tb_chat_history(
+    id UUID PRIMARY KEY,
+    session_id UUID NOT NULL,
+    content TEXT NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    user_id UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES tb_users(id)
+);
