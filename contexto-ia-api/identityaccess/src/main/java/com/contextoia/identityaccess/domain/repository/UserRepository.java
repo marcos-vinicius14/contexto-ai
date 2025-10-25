dcf344858bed68d5986b3516c213191b9febface
marcos <marcos.vinicius.dev3@gmail.com>
2025-10-24 21:15:46 -0300

    feat: finish UserDetailsServiceImpl

--- contexto-ia-api/identityaccess/src/main/java/com/contextoia/identityaccess/domain/repository/UserRepository.java
+++ contexto-ia-api/identityaccess/src/main/java/com/contextoia/identityaccess/domain/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.contextoia.identityaccess.domain.repository;
 
+import com.contextoia.identityaccess.application.dto.UserAuthDetails;
 import com.contextoia.identityaccess.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository  extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+    Optional<UserAuthDetails> findAuthDetailsByUsername(String username);
 }
 
 
